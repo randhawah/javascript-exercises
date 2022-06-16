f6178656b6caadbe92146715bce31d54d3466ff1
Harman Randhawa <54494701+randhawah@users.noreply.github.com>
2022-06-16 19:46:31 -0400

    Complete javascript exercises from 01 to 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    
+    //return arr.filter(x => !args.includes(x));
 
+    args.forEach((arg) => {
+        const index = arr.indexOf(arg);
+        if (index > -1)
+          arr.splice(index, 1);
+      });
+    
+/*
+      args.forEach(removeArg);
+
+      function removeArg(item){
+        const index = arr.indexOf(item);
+        if (index > -1)
+          arr.splice(index, 1);
+      }
+*/
+      return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if(start < 0 || end < 0 || typeof start != 'number' || typeof end != 'number'){
+        sum = 'ERROR';
+    }
+    else if(start>end){
+        for(let i=start; i>=end; i--)
+            sum += i;
+    }
+    else{
+        for(let i=start; i<=end; i++)
+            sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
